5a3248f643a1f0bd0a1cbc766eacc856ba7d8173
alxmcs <70561974+alxmcs@users.noreply.github.com>
2022-05-13 14:45:44 +0400

    logging added

--- main.py
+++ main.py
@@ -1,21 +1,34 @@
 from imutils.video import VideoStream
-import imutils
-import time
-import pickle
-import json
 from utils.voice import VoiceEmitter
 import face_recognition
+import logging
+import imutils
+import pickle
+import json
+import time
 
 SETTINGS_PATH = 'settings.json'
 
 
 class PiButler:
     def __init__(self, settings_path):
+        logging.basicConfig(handlers=[logging.FileHandler(filename="PiButler.log",
+                                                          encoding='utf-8', mode='a+')],
+                            format="%(asctime)s %(name)s:%(levelname)s:%(message)s",
+                            datefmt="%m/%d/%Y %I:%M:%S",
+                            level=logging.INFO)
+        logging.info('Begin initialization')
         with open(settings_path, encoding="utf-8") as json_file:
             settings = json.load(json_file)
+        self.__timeout = settings['timeout']
+        logging.info('Settings read')
         self.__data = pickle.loads(open(settings['encodings_path'], "rb").read())
+        logging.info('Encodings read')
         self.__emitter = VoiceEmitter(settings['phrases_list'], settings['unknown_name'], settings['replace_symbol'])
+        self.__emitter.play_message(settings['startup_message'])
+        logging.info('Text-to-speech initialized')
         self.__vs = VideoStream(src=0).start()
+        logging.info('Video stream capture started')
 
     def run_camera(self):
         time.sleep(2.0)
@@ -24,6 +37,7 @@ class PiButler:
             frame = imutils.resize(frame, width=500)
             boxes = face_recognition.face_locations(frame)
             encodings = face_recognition.face_encodings(frame, boxes)
+            logging.info(f'got {boxes.count()} faces')
             for encoding in encodings:
                 matches = face_recognition.compare_faces(self.__data["encodings"], encoding)
                 if True in matches:
@@ -34,9 +48,11 @@ class PiButler:
                         counts[name] = counts.get(name, 0) + 1
                     name = max(counts, key=counts.get)
                     self.__emitter.play_greeting(name)
+                    logging.info(f'recognized {name}')
                 else:
                     self.__emitter.play_greeting(None)
-            time.sleep(5.0)
+                    logging.info('unknown person appeared')
+            time.sleep(self.__timeout)
 
 
 if __name__ == "__main__":
