b76c99f1c73925473b955839e8c8af644e1b4b2f
alxmcs <70561974+alxmcs@users.noreply.github.com>
2022-11-01 23:49:28 +0400

    Update encodings.py

--- utils/encodings.py
+++ utils/encodings.py
@@ -12,7 +12,7 @@ import os
 class FaceEncoder:
     __extensions = ['.jpg', '.png', '.bmp', '.gif', '.tiff']
 
-    def __init__(self, image_folder_path, names_json_path, encodings_pickle_path):
+    def __init__(self, image_folder_path, metadata_json_path, encodings_pickle_path):
         logging.basicConfig(handlers=[logging.StreamHandler(sys.stdout),
                                       logging.FileHandler(
                                           filename=os.path.join(os.path.dirname(os.getcwd()), "Encoding.log"),
@@ -22,8 +22,8 @@ class FaceEncoder:
                             level=logging.INFO)
         self.__new_images_path = image_folder_path
         logging.info(f'got path to folder with new images- {self.__new_images_path}')
-        self.__new_names_path = names_json_path
-        logging.info(f'got path to json with new faces metadata - {self.__new_names_path}')
+        self.__new_metadata_path = metadata_json_path
+        logging.info(f'got path to json with new faces metadata - {self.__new_metadata_path}')
         self.__encodings_path = encodings_pickle_path
         logging.info(f'got path to encodings pickle - {self.__encodings_path}')
 
@@ -32,8 +32,8 @@ class FaceEncoder:
         logging.info(f'Got {len(images)} paths to new images')
         return images
 
-    def _get_new_names(self):
-        with open(self.__new_names_path, encoding='utf-8') as json_file:
+    def _get_new_metadata(self):
+        with open(self.__new_metadata_path, encoding='utf-8') as json_file:
             names_dict = json.load(json_file)
         logging.info(f'Got metadata of {len(names_dict)} new faces')
         return names_dict
@@ -46,33 +46,29 @@ class FaceEncoder:
             encodings = data["encodings"]
             logging.info(
                 f'Got {len(encodings)} encodings '
-                f'for {len(set(tuple(f) for f in names))} people'
-                f' from {self.__encodings_path}')
+                f'for {len(set(tuple(f) for f in names))} people '
+                f'from {self.__encodings_path}')
             return encodings, names
         else:
             return None
 
-    def generate_encodings(self,image_folder, names_path, encodings_path):
+    def _write_encodings(self, encodings, names, encodings_path=None):
+        if not encodings_path:
+            encodings_path = self.__encodings_path
+        with open(encodings_path, "wb") as pickle_file:
+            names_list = [[f['name'], f['sign']] for f in names]
+            pickle.dump({"encodings": encodings, "names": names_list}, pickle_file)
+        logging.info(f'Wrote {len(encodings)} encodings '
+                     f'for {len(set(tuple(f) for f in names_list))} people '
+                     f'saved in {encodings_path}')
 
-        encodings = []
-        names = []
-        image_paths = [p for p in paths.list_images(image_folder) if os.path.splitext(p)[1] in EXTENSIONS]
-        logging.info(f'Got {len(image_paths)} paths to images')
-        with open(names_path, encoding='utf-8') as json_file:
-            names_dict = json.load(json_file)
-        logging.info(f'Read file:person dictionary of {len(names_dict)} elements')
-
-        if os.path.exists(encodings_path) and os.stat(encodings_path).st_size != 0:
-            with open(encodings_path, "rb") as pickle_file:
-                data = pickle.loads(pickle_file.read())
-            names = [{'name': f[0], 'sign': f[1]} for f in data["names"]]
-            encodings = data["encodings"]
-            logging.info(
-                f'Got {len(encodings)} encodings for {len(set(tuple(f) for f in data["names"]))} people from {encodings_path}')
-
-        for (i, image_path) in enumerate(image_paths):
-            if os.path.splitext(os.path.basename(image_path))[0] in names_dict.keys():
-                new_name = names_dict[os.path.splitext(os.path.basename(image_path))[0]]
+    def generate_encodings(self):
+        encodings, names = self._get_encodings_data()
+        metadata = self._get_new_metadata()
+        images = self._get_new_images()
+        for (i, image_path) in enumerate(images):
+            if os.path.splitext(os.path.basename(image_path))[0] in metadata.keys():
+                new_person = metadata[os.path.splitext(os.path.basename(image_path))[0]]
                 with open(image_path, "rb") as f:
                     chunk = f.read()
                 chunk_arr = np.frombuffer(chunk, dtype=np.uint8)
@@ -82,19 +78,15 @@ class FaceEncoder:
                 new_encodings = face_recognition.face_encodings(rgb, boxes)
                 for encoding in new_encodings:
                     encodings.append(encoding)
-                    names.append(new_name)
-                    logging.info(f'Added encoding for {new_name}')
-
-        with open(encodings_path, "wb") as pickle_file:
-            names_list = [[f['name'], f['sign']] for f in names]
-            pickle.dump({"encodings": encodings, "names": names_list}, pickle_file)
-        logging.info(
-            f'{len(encodings)} encodings for {len(set(tuple(f) for f in names_list))} people saved in {encodings_path}')
+                    names.append(new_person)
+                    logging.info(f'Added encoding for {new_person}')
+        self._write_encodings(encodings, names)
 
 
 if __name__ == "__main__":
     with open(os.path.join(os.path.dirname(os.getcwd()), 'settings.json'), encoding="utf-8") as settings_file:
         settings = json.load(settings_file)
-    generate_encodings(os.path.join(os.path.dirname(os.getcwd()), settings['images_path']),
-                       os.path.join(os.path.dirname(os.getcwd()), settings['names_path']),
-                       os.path.join(os.path.dirname(os.getcwd()), settings['encodings_path']))
+    encoder = FaceEncoder(os.path.join(os.path.dirname(os.getcwd()), settings['images_path']),
+                          os.path.join(os.path.dirname(os.getcwd()), settings['names_path']),
+                          os.path.join(os.path.dirname(os.getcwd()), settings['encodings_path']))
+    encoder.generate_encodings()
